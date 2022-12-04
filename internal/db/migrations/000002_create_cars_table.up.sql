f2e6819b3000e54f2a641308cb92005bb1c92769
naman-dave <namandave@duck.com>
2022-12-04 22:10:18 +0530

    fix: remove the unwanted field from migration

--- internal/db/migrations/000002_create_cars_table.up.sql
+++ internal/db/migrations/000002_create_cars_table.up.sql
@@ -3,8 +3,6 @@ CREATE TABLE IF NOT EXISTS Cars (
     CarIdentifier VARCHAR (127) NOT NULL UNIQUE,
     Name VARCHAR (127) NULL,
     DateOfManufacture DATE NULL,
-    LastServiceDate DATE NULL,
-    LastUsedDate DATE NULL,
     Total INT NOT NULL,
     TotalInUse INT NOT NULL
 )
